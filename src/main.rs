6205f6a306f41019cc1c6b157a4b823a91aaa3df
Enrico Franchi <enrico.franchi@gmail.com>
2018-04-25 20:53:37 +0100

    Starts implementing num parser.

--- src/main.rs
+++ src/main.rs
@@ -68,6 +68,7 @@ enum ParserErrorType {
 enum ParserState {
     TopLevel,
     Error(usize, ParserErrorType),
+    Num{start: usize, end: usize, seen_dot: bool},
     End,
 }
 
@@ -76,38 +77,6 @@ struct ParserError {
     position: usize,
     error_type: ParserErrorType,
 }
-
-
-
-fn byte_to_instruction<T>(ch: u8) -> Option<Instruction<T>> {
-    match ch {
-        0 => Some(Instruction::Nop),
-        b'p' => Some(Instruction::PrintLN),
-        b'n' => Some(Instruction::PrintPop),
-        b'P' => Some(Instruction::PrettyPrint),
-        b'f' => Some(Instruction::PrintStack),
-        b'+' => Some(Instruction::Add),
-        b'-' => Some(Instruction::Sub),
-        b'*' => Some(Instruction::Mul),
-        b'/' => Some(Instruction::Div),
-        b'%' => Some(Instruction::Mod),
-        b'~' => Some(Instruction::Divmod),
-        b'^' => Some(Instruction::Exp),
-        b'|' => Some(Instruction::Modexp),
-        b'v' => Some(Instruction::Sqrt),
-        b'c' => Some(Instruction::Clear),
-        b'd' => Some(Instruction::Dup),
-        b'r' => Some(Instruction::Swap),
-        b'i' => Some(Instruction::SetInputRadix),
-        b'o' => Some(Instruction::SetOutputRadix),
-        b'k' => Some(Instruction::SetPrecision),
-        b'I' => Some(Instruction::GetInputRadix),
-        b'O' => Some(Instruction::GetOutputRadix),
-        b'K' => Some(Instruction::GetPrecision),
-        _ => None
-    }
-}
-
 
 fn parse<T>(program_text: &[u8]) -> Result<Vec<Instruction<T>>, ParserError> {
     let mut state = ParserState::TopLevel;
@@ -118,39 +87,83 @@ fn parse<T>(program_text: &[u8]) -> Result<Vec<Instruction<T>>, ParserError> {
         return Ok(instructions);
     }
     loop {
-        if let ParserState::Error(position, error_type) = state {
-            return Err(ParserError{position, error_type});
-        }
         if col >= program_text.len() {
             state = ParserState::End;
-            break;
         }
 
-        let ch = program_text[col];
-        match ch {
-            0 => {
-                match byte_to_instruction(ch) {
-                    Some(instruction) => {
-                        instructions.push(instruction);
-                    },
-                    None => {
-                        state = ParserState::Error(col, ParserErrorType::IllegalState("error in byte_to_instruction or match".to_string()))
+        match state {
+            ParserState::End => {
+                break
+            }
+            ParserState::Error(_, _) => {
+                break
+            }
+            ParserState::TopLevel => {
+                let ch = program_text[col];
+
+                match ch {
+                    0 => instructions.push(Instruction::Nop),
+                    b'p' => instructions.push(Instruction::PrintLN),
+                    b'n' => instructions.push(Instruction::PrintPop),
+                    b'P' => instructions.push(Instruction::PrettyPrint),
+                    b'f' => instructions.push(Instruction::PrintStack),
+                    b'+' => instructions.push(Instruction::Add),
+                    b'-' => instructions.push(Instruction::Sub),
+                    b'*' => instructions.push(Instruction::Mul),
+                    b'/' => instructions.push(Instruction::Div),
+                    b'%' => instructions.push(Instruction::Mod),
+                    b'~' => instructions.push(Instruction::Divmod),
+                    b'^' => instructions.push(Instruction::Exp),
+                    b'|' => instructions.push(Instruction::Modexp),
+                    b'v' => instructions.push(Instruction::Sqrt),
+                    b'c' => instructions.push(Instruction::Clear),
+                    b'd' => instructions.push(Instruction::Dup),
+                    b'r' => instructions.push(Instruction::Swap),
+                    b'i' => instructions.push(Instruction::SetInputRadix),
+                    b'o' => instructions.push(Instruction::SetOutputRadix),
+                    b'k' => instructions.push(Instruction::SetPrecision),
+                    b'I' => instructions.push(Instruction::GetInputRadix),
+                    b'O' => instructions.push(Instruction::GetOutputRadix),
+                    b'K' => instructions.push(Instruction::GetPrecision),
+                    ch => {
+                        state = ParserState::Error(col, ParserErrorType::InvalidCharacter(ch));
+                        continue;
                     }
                 }
+
+                col += 1;
             }
-            ch => {
-                state = ParserState::Error(col, ParserErrorType::InvalidCharacter(ch))
-                
+            ParserState::Num{start, end, seen_dot} => {
+                let ch = program_text[col];
+                match (seen_dot, ch) {
+                    (true, b'.') => {
+
+                    }
+                    (false, b'.') => {
+
+                    }
+                    (_, b'0' ... b'9') => {
+
+
+                    }
+                    _ => {
+
+                    }
+                }
+
             }
         }
-
-        col += 1;
+        
     }
 
     return match state {
         ParserState::Error(position, error_type) => Err(ParserError{position, error_type}),
         ParserState::End => Ok(instructions),
-        ParserState::TopLevel => Err(ParserError{position: col, error_type: ParserErrorType::IllegalState("parsing stopped".to_string())})
+        ParserState::TopLevel => Err(ParserError{position: col, error_type: ParserErrorType::IllegalState("parsing stopped".to_string())}),
+        _other => {
+
+            Err(ParserError{position: col, error_type: ParserErrorType::IllegalState("not sure".to_string())})
+        }
     }
 }
 
@@ -170,6 +183,28 @@ parse_tests! {
     parse_test_empty: ("", Ok(vec![])),
     parse_test_invalid: ("z", Err(ParserError{position: 0, error_type: ParserErrorType::InvalidCharacter(b'z')})),
     parse_test_zero: ("\0", Ok(vec![Instruction::Nop])),
+    print_test_p: ("p", Ok(vec![Instruction::PrintLN])),
+    print_test_n: ("n", Ok(vec![Instruction::PrintPop])),
+    print_test_p2: ("P", Ok(vec![Instruction::PrettyPrint])),
+    print_test_f: ("f", Ok(vec![Instruction::PrintStack])),
+    print_test_add: ("+", Ok(vec![Instruction::Add])),
+    print_test_sub: ("-", Ok(vec![Instruction::Sub])),
+    print_test_mul: ("*", Ok(vec![Instruction::Mul])),
+    print_test_div: ("/", Ok(vec![Instruction::Div])),
+    print_test_mod: ("%", Ok(vec![Instruction::Mod])),
+    print_test_divmod: ("~", Ok(vec![Instruction::Divmod])),
+    print_test_exp: ("^", Ok(vec![Instruction::Exp])),
+    print_test_expmod: ("|", Ok(vec![Instruction::Modexp])),
+    print_test_v: ("v", Ok(vec![Instruction::Sqrt])),
+    print_test_c: ("c", Ok(vec![Instruction::Clear])),
+    print_test_d: ("d", Ok(vec![Instruction::Dup])),
+    print_test_r: ("r", Ok(vec![Instruction::Swap])),
+    print_test_i: ("i", Ok(vec![Instruction::SetInputRadix])),
+    print_test_o: ("o", Ok(vec![Instruction::SetOutputRadix])),
+    print_test_k: ("k", Ok(vec![Instruction::SetPrecision])),
+    print_test_i2: ("I", Ok(vec![Instruction::GetInputRadix])),
+    print_test_o2: ("O", Ok(vec![Instruction::GetOutputRadix])),
+    print_test_k2: ("K", Ok(vec![Instruction::GetPrecision])),
 }
 
 #[test]
