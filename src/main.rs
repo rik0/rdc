419985eb8b704443c70a597b2616827dac922e16
Enrico Franchi <enrico.franchi@gmail.com>
2018-04-25 19:39:14 +0100

    Working parser structure.

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,13 @@
+use std::io::Write;
 
-enum Cell<T> {
-    N(T),
-    S(String)
-}
-
+#[derive(Copy, Clone, Debug,PartialEq)]
 enum Instruction<T> {
     Nop,
     Num(T),
     // print
     PrintLN,
     PrintPop,
+    PrettyPrint,
     PrintStack,
     // arithmetic
     Add,
@@ -60,39 +58,169 @@ enum Instruction<T> {
     GetArray
 }
 
-struct Program {
-
+#[derive(Clone,Debug,PartialEq)]
+enum ParserErrorType {
+    IllegalState(String),
+    InvalidCharacter(u8),
 }
 
-enum Parser {
-    TopLevelParser,
-
+#[derive(Clone,Debug,PartialEq)]
+enum ParserState {
+    TopLevel,
+    Error(usize, ParserErrorType),
+    End,
 }
 
-struct ParserState<'a, T> {
-    parser: Parser,
-    instructions: Vec<Instruction<T>>,
-    rest: &'a str
+#[derive(Clone,Debug,PartialEq)]
+struct ParserError {
+    position: usize,
+    error_type: ParserErrorType,
 }
 
-impl<'a, T> ParserState<'a, T> {
-    fn parse_next(self) -> ParserState<'a, T> {
-        return self
+
+
+fn byte_to_instruction<T>(ch: u8) -> Option<Instruction<T>> {
+    match ch {
+        0 => Some(Instruction::Nop),
+        b'p' => Some(Instruction::PrintLN),
+        b'n' => Some(Instruction::PrintPop),
+        b'P' => Some(Instruction::PrettyPrint),
+        b'f' => Some(Instruction::PrintStack),
+        b'+' => Some(Instruction::Add),
+        b'-' => Some(Instruction::Sub),
+        b'*' => Some(Instruction::Mul),
+        b'/' => Some(Instruction::Div),
+        b'%' => Some(Instruction::Mod),
+        b'~' => Some(Instruction::Divmod),
+        b'^' => Some(Instruction::Exp),
+        b'|' => Some(Instruction::Modexp),
+        b'v' => Some(Instruction::Sqrt),
+        b'c' => Some(Instruction::Clear),
+        b'd' => Some(Instruction::Dup),
+        b'r' => Some(Instruction::Swap),
+        b'i' => Some(Instruction::SetInputRadix),
+        b'o' => Some(Instruction::SetOutputRadix),
+        b'k' => Some(Instruction::SetPrecision),
+        b'I' => Some(Instruction::GetInputRadix),
+        b'O' => Some(Instruction::GetOutputRadix),
+        b'K' => Some(Instruction::GetPrecision),
+        _ => None
     }
 }
 
 
-fn parse<T>(program_text: &str) -> Program {
-    let mut p = ParserState::<T>{ parser: Parser::TopLevelParser, instructions: Vec::new(), rest: program_text};
+fn parse<T>(program_text: &[u8]) -> Result<Vec<Instruction<T>>, ParserError> {
+    let mut state = ParserState::TopLevel;
+    let mut instructions = Vec::new();
+    let mut col: usize = 0;
 
-    while p.rest.len() > 0 {
-        p = p.parse_next();
+    if program_text.len() == 0 {
+        return Ok(instructions);
+    }
+    loop {
+        if let ParserState::Error(position, error_type) = state {
+            return Err(ParserError{position, error_type});
+        }
+        if col >= program_text.len() {
+            state = ParserState::End;
+            break;
+        }
+
+        let ch = program_text[col];
+        match ch {
+            0 => {
+                match byte_to_instruction(ch) {
+                    Some(instruction) => {
+                        instructions.push(instruction);
+                    },
+                    None => {
+                        state = ParserState::Error(col, ParserErrorType::IllegalState("error in byte_to_instruction or match".to_string()))
+                    }
+                }
+            }
+            ch => {
+                state = ParserState::Error(col, ParserErrorType::InvalidCharacter(ch))
+                
+            }
+        }
+
+        col += 1;
     }
 
-    Program{}
+    return match state {
+        ParserState::Error(position, error_type) => Err(ParserError{position, error_type}),
+        ParserState::End => Ok(instructions),
+        ParserState::TopLevel => Err(ParserError{position: col, error_type: ParserErrorType::IllegalState("parsing stopped".to_string())})
+    }
+}
+
+macro_rules! parse_tests {
+    ($($name:ident: $value:expr,)*) => {
+    $(
+        #[test]
+        fn $name() {
+            let (input, expected) = $value;
+            assert_eq!(expected, parse::<f64>(input.as_bytes()));
+        }
+    )*
+    }
+}
+
+parse_tests! {
+    parse_test_empty: ("", Ok(vec![])),
+    parse_test_invalid: ("z", Err(ParserError{position: 0, error_type: ParserErrorType::InvalidCharacter(b'z')})),
+    parse_test_zero: ("\0", Ok(vec![Instruction::Nop])),
+}
+
+#[test]
+fn testparse() {
+    let (input, expected) = ("", Ok(vec![]));
+    assert_eq!(expected, parse::<f64>(input.as_bytes()));
 }
 
 
 fn main() {
+    match parse::<f64>(&("".as_bytes())) {
+        Ok(instructions) => { println!("{:?}", instructions) },
+        Err(ParserError{position, error_type}) => {
+            writeln!(std::io::stderr(), "{:?} at col {}", error_type, position).unwrap();
+            //std::process:exit(1);
+        }
+    }
     println!("Hello, world!");
 }
+
+// impl<'a, T> Parser<'a, T> {
+//     fn parse(mut &self) {
+//         while self.col < self.program_text.len()  {
+//             match self.parser {
+//                 ParserState::TopLevel => {
+//                     let current = self.program_text[self.col];
+//                     match current {
+//                         0 => {
+//                             match byte_to_instruction(current) {
+//                                 Some(instruction) => {
+//                                     self.instructions.push(instruction);
+//                                     self.col += 1;
+//                                 }
+//                                 None => {
+//                                     self.parser = ParserState::Error(self.col, ErrorType::IllegalState);
+//                                 }
+//                             }
+//                         }
+//                         _ => {
+
+//                         }
+//                     }
+
+//                 }
+//                 ParserState::Error(_, _) => {
+//                     return 
+//                 }
+//                 ParserState::End => {
+//                     return
+//                 }
+//             }
+//         }
+//     }
+// }
